80609a9909db89dda4d9a72e7c1d7676ae026a19
mmariano <marcusney@gmail.com>
2019-02-28 08:42:35 -0300

    First project release

---
+++ calc.py
@@ -0,0 +1,18 @@
+"""Modulo cal"""
+
+
+class Calc():
+    """Modulo cal"""
+    def soma(self, x: int, y: int):
+        """Modulo cal"""
+        return x + y
+    
+    def sub(self, x: int, y: int):
+        """Modulo cal"""
+        return x - y
+    
+calc = Calc()
+
+print(calc.soma(15, 25))
+
+print(calc.sub(15, 25))
